9f4cdde11304137409785565689cdf2d33be336b
dgotlieb <Dgotlieb8@gmail.com>
2019-07-05 14:28:01 +0300

    Update DB.java

--- DB.java
+++ DB.java
@@ -1,10 +1,11 @@
 import java.sql.*;
 
 public class DB {
+    private static final String USER_NAME = szswnaapzk;
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(
-                "jdbc:mysql://remotemysql.com:3306", "<user>", "<password>");
+                "jdbc:mysql://remotemysql.com:3306", USER_NAME, "6mgTl1Slou");
         Statement stmt = con.createStatement();
 //        createTable(stmt);
 //        insertStudent(con, 123, "daniel");
@@ -12,19 +13,19 @@ public class DB {
     }
 
     private static void createTable(Statement stmt) throws SQLException {
-        String statementToExecute2 = "CREATE TABLE `szswnaapzk`.`users`(`id` INT NOT NULL,`name` VARCHAR(45) NOT NULL, PRIMARY KEY (`id`));";
+        String statementToExecute2 = "CREATE TABLE '"+USER_NAME+"'.`users`(`id` INT NOT NULL,`name` VARCHAR(45) NOT NULL, PRIMARY KEY (`id`));";
         stmt.execute(statementToExecute2);
     }
 
     private static void insertStudent(Connection con, int id, String name) throws SQLException {
         String statementToExecute = "";
         Statement stmt = con.createStatement();
-        statementToExecute = "INSERT INTO `szswnaapzk`.`users` (`id`, `name`) VALUES ('" + id + "', '" + name + "');";
+        statementToExecute = "INSERT INTO '"+USER_NAME+"'.`users` (`id`, `name`) VALUES ('" + id + "', '" + name + "');";
         stmt.execute(statementToExecute);
     }
 
     private static void getTableContent(Statement stmt) throws SQLException {
-        String statementToExecute = "SELECT * FROM szswnaapzk.users;";
+        String statementToExecute = "SELECT * FROM "+USER_NAME+".users;";
         ResultSet rs = stmt.executeQuery(statementToExecute);
         while(rs.next()){
             //Retrieve by column name
